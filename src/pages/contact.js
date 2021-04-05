539ff246b6e40719222cc40cf92270e2b0f01beb
Angger Bayu Pangestu <kontak@angger.me>
2021-04-05 15:39:58 +0700

    using graphql

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Contact = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          email
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <div>
         <h1>Contact Me</h1>
+        <p>Email {data.site.siteMetadata.email}</p>
+
         <p>Diisi nanti............</p>
       </div>
     </Layout>
