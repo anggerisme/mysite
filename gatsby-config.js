c7a42b066f288d29d5d9e444716f84497e5a443d
Angger Bayu Pangestu <kontak@angger.me>
2021-04-05 15:41:09 +0700

    using graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: `Angger's Blog`,
+    nama: `angger`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Anggerisme`,
+    email: `kontak@angger.me`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
