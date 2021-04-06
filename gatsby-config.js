9317a9ee7a64e99f52a983d715b41a68de048ade
Angger Bayu Pangestu <kontak@angger.me>
2021-04-06 12:24:11 +0700

    connecting to contentfull

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
     email: `kontak@angger.me`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,17 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
+            title
+            slug
+            publishedDate(fromNow: true)
           }
         }
       }
@@ -22,11 +22,13 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol>
-        {data.allMarkdownRemark.edges.map(edge => {
+        {data.allContentfulBlogPost.edges.map(edge => {
           return (
             <li>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publisheDate}</p>
+              </Link>
             </li>
           )
         })}
